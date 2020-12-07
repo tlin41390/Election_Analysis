6e5d32466f7789068403999af13ea2c0ebf01e3a
tlin41390 <56329045+tlin41390@users.noreply.github.com>
2020-12-06 21:04:15 -0800

    Delete Python_practice.py

--- Python_practice.py
+++
@@ -1,7 +0,0 @@
-
-counties = ["Arapahoe","Denver","Jefferson"]
-
-counties_dict ={"Arapahoe":422829, "Denver": 463353,"Jefferson": 432438}
-
-for counties,values in counties_dict.items():
-    print(counties +" county has "+ str(values)+ " registered voters")
